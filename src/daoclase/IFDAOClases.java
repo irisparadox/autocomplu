6d8e1e2cd9e5a56eb1f5ebbe43dfb02ddf06c5a5
stepatik <stepatik@ucm.es>
2025-04-16 13:12:44 +0200

    Update IFDAOClases.java

--- src/daoclase/IFDAOClases.java
+++ src/daoclase/IFDAOClases.java
@@ -1,5 +1,17 @@
 package daoclase;
 
-public interface IFDAOClases {
+import java.util.List;
 
+import dominio.Clase;
+
+public interface IFDAOClases {
+	public Clase consultarClase(String idClase);
+	public List<Clase> consultarClasesAlumno(String idAlumno);
+	public List<Clase> consultarClasesProfesor(String idProfesor);
+	public int crearClase(Clase nuevaClase);
+	public int existeClase(String idCorreo);
+	public int editarClase(Clase clase);
+	public int eliminarClase(String idClase);
+	public int hayAlumno(String idClase);
+	public int hayProfesor(String idClase);
 }
