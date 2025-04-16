68554e093fa4213b948774d8c4c38587c6e508f3
elvergchueca <adanielo@ucm.es>
2025-04-16 14:30:08 +0200

    DAOClases.java

--- src/daoclase/DAOClases.java
+++ src/daoclase/DAOClases.java
@@ -15,11 +15,13 @@ public class DAOClases implements IDAOClases {
 	public int crearClase(Clase clase) {
 		int success;
 		try (Connection connection = DriverManager.getConnection(DatabaseConfig.DB_URL, DatabaseConfig.DB_USER, DatabaseConfig.DB_PASSWORD)) {
-			String query = "INSERT INTO clases VALUES (?, ?, ?)";
+			String query = "INSERT INTO clases VALUES (?, ?, ?, ?)";
 			PreparedStatement statement = connection.prepareStatement(query);
-			statement.setString(1, clase.getAlumno().getIdCorreo());
-			statement.setString(2, clase.getProfesor().getIdCorreo());
-			statement.setDate(3, new java.sql.Date(clase.getFecha().getTime()));
+			
+			statement.setString(1, clase.getIdClase());
+			statement.setString(2, clase.getAlumno().getIdCorreo());
+			statement.setString(3, clase.getProfesor().getIdCorreo());
+			statement.setDate(4, new java.sql.Date(clase.getFecha().getTime()));
 			
 			int rowsInserted = statement.executeUpdate();
 			success = rowsInserted > 0 ? 0x0 : 0x1;
@@ -202,4 +204,43 @@ public class DAOClases implements IDAOClases {
 		}
 		return existe;
 	}
+
+	@Override
+	public int hayClaseAlumnoHora(String idAlumno, java.sql.Date fecha) {
+		int existe = 0x1;
+		try (Connection connection = DriverManager.getConnection(DatabaseConfig.DB_URL, DatabaseConfig.DB_USER, DatabaseConfig.DB_PASSWORD)) {
+			String query = "SELECT COUNT(*) FROM clases WHERE idCorreoAlumno = ? AND horario = ?";
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, idAlumno);
+			statement.setDate(2, fecha);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next() && rs.getInt(1) > 0) {
+				existe = 0x0;
+			}
+		} catch (SQLException e) {
+			existe = -0x1;
+			e.printStackTrace();
+		}
+		return existe;
+	}
+
+	@Override
+	public int hayClaseProfesorHora(String idProfesor, java.sql.Date fecha) {
+		int existe = 0x1;
+		try (Connection connection = DriverManager.getConnection(DatabaseConfig.DB_URL, DatabaseConfig.DB_USER, DatabaseConfig.DB_PASSWORD)) {
+			String query = "SELECT COUNT(*) FROM clases WHERE idCorreoProfesor = ? AND horario = ?";
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, idProfesor);
+			statement.setDate(2, fecha); 
+			ResultSet rs = statement.executeQuery();
+			if (rs.next() && rs.getInt(1) > 0) {
+				existe = 0x0;
+			}
+		} catch (SQLException e) {
+			existe = -0x1;
+			e.printStackTrace();
+		}
+		return existe;
+	}
+
 }
