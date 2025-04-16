9e748d56754f4e2d9d72f0c0d49d1a6b77ef6c2d
elvergchueca <adanielo@ucm.es>
2025-04-16 14:28:19 +0200

    Update IFSAClases.java

--- src/saclases/IFSAClases.java
+++ src/saclases/IFSAClases.java
@@ -1,5 +1,20 @@
 package saclases;
 
+import java.sql.Date;
+import java.util.List;
+
+import dominio.Alumno;
+import dominio.Clase;
+import dominio.Profesor;
+
 public interface IFSAClases {
 
+	
+	public int createClase(Alumno idAlumno, String idClase, Date fecha,Profesor prof);
+	public int eliminarClase(String idClase);
+	public Clase consultarClase(String idClase);
+	public List<Clase> consultarClasesAlumno(String idAlumno);
+	public List<Clase> consultarClasesProfesor(String idProfesor);
+	public int editarClase(Clase clas);
+	
 }
